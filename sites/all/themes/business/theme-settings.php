6171ae91d8bceabfe64df2122ba50a0fd2bd7bec
Jason Bethel <jlbethel@gmail.com>
2015-11-05 16:48:56 -0800

    create custome theme

--- sites/all/themes/business/theme-settings.php
+++ sites/all/themes/business/theme-settings.php
@@ -7,24 +7,24 @@
  * @param $form_state
  *   The form state.
  */
-function business_form_system_theme_settings_alter(&$form, &$form_state) {
+function my_business_form_system_theme_settings_alter(&$form, &$form_state) {
 
   $form['busi_settings'] = array(
     '#type' => 'fieldset',
-    '#title' => t('Business Theme Settings'),
+    '#title' => t('my_business Theme Settings'),
     '#collapsible' => FALSE,
     '#collapsed' => FALSE,
   );
   $form['busi_settings']['show_front_content'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show content and sidebar on front page'),
-    '#default_value' => theme_get_setting('show_front_content','business'),
+    '#default_value' => theme_get_setting('show_front_content','my_business'),
     '#description' => t('Check this option to show content and sidebar on the front page.'),
   );
   $form['busi_settings']['breadcrumbs'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show breadcrumbs in a page'),
-    '#default_value' => theme_get_setting('breadcrumbs','business'),
+    '#default_value' => theme_get_setting('breadcrumbs','my_business'),
     '#description'   => t("Check this option to show breadcrumbs in page. Uncheck to hide."),
   );
   $form['busi_settings']['slideshow'] = array(
@@ -36,7 +36,7 @@ function business_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['busi_settings']['slideshow']['slideshow_display'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show slideshow'),
-    '#default_value' => theme_get_setting('slideshow_display','business'),
+    '#default_value' => theme_get_setting('slideshow_display','my_business'),
     '#description'   => t("Check this option to show Slideshow in front page. Uncheck to hide."),
   );
     $form['busi_settings']['slideshow']['slide'] = array(
@@ -51,12 +51,12 @@ function business_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['busi_settings']['slideshow']['slide1']['slide1_desc'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide1_desc','business'),
+    '#default_value' => theme_get_setting('slide1_desc','my_business'),
   );
   $form['busi_settings']['slideshow']['slide1']['slide1_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide1_url','business'),
+    '#default_value' => theme_get_setting('slide1_url','my_business'),
   );
   $form['busi_settings']['slideshow']['slide2'] = array(
     '#type' => 'fieldset',
@@ -67,12 +67,12 @@ function business_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['busi_settings']['slideshow']['slide2']['slide2_desc'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide2_desc','business'),
+    '#default_value' => theme_get_setting('slide2_desc','my_business'),
   );
   $form['busi_settings']['slideshow']['slide2']['slide2_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide2_url','business'),
+    '#default_value' => theme_get_setting('slide2_url','my_business'),
   );
   $form['busi_settings']['slideshow']['slide3'] = array(
     '#type' => 'fieldset',
@@ -83,15 +83,15 @@ function business_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['busi_settings']['slideshow']['slide3']['slide3_desc'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide3_desc','business'),
+    '#default_value' => theme_get_setting('slide3_desc','my_business'),
   );
   $form['busi_settings']['slideshow']['slide3']['slide3_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide3_url','business'),
+    '#default_value' => theme_get_setting('slide3_url','my_business'),
   );
   $form['busi_settings']['slideshow']['slideimage'] = array(
-    '#markup' => t('To change the Slide Images, Replace the slide-image-1.jpg, slide-image-2.jpg and slide-image-3.jpg in the images folder of the Business theme folder.'),
+    '#markup' => t('To change the Slide Images, Replace the slide-image-1.jpg, slide-image-2.jpg and slide-image-3.jpg in the images folder of the my_business theme folder.'),
   );
   $form['busi_settings']['footer'] = array(
     '#type' => 'fieldset',
@@ -102,13 +102,13 @@ function business_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['busi_settings']['footer']['footer_copyright'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show copyright text in footer'),
-    '#default_value' => theme_get_setting('footer_copyright','business'),
+    '#default_value' => theme_get_setting('footer_copyright','my_business'),
     '#description'   => t("Check this option to show copyright text in footer. Uncheck to hide."),
   );
   $form['busi_settings']['footer']['footer_credits'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show theme credits in footer'),
-    '#default_value' => theme_get_setting('footer_credits','business'),
+    '#default_value' => theme_get_setting('footer_credits','my_business'),
     '#description'   => t("Check this option to show copyright text in footer. Uncheck to hide."),
   );
 }
